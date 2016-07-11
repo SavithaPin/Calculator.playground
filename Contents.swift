b0ff1d29c4dc3be7b035c9e4d07e88e249d01f25
SavithaPin <prc-lh138-04@PRC-LH138-04s-iMac.local>
2016-07-11 15:51:17 -0500

    Calculator

---
+++ Contents.swift
@@ -0,0 +1,20 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Calculator"
+
+let num1 = 3
+let num2 = 5
+
+let add = num1 + num2
+let sub = num1 - num2
+let mul = num1 * num2
+let div = num1 / num2
+let mod = num1 % num2
+
+print("Add = \(add)",terminator: " ")
+print("Sub = \(sub)",terminator: " ")
+print("Mul = \(mul)", terminator: " ")
+print("Div = \(div)", terminator: " ")
+print("Mod = \(mod)", terminator: " ")
